6517d75025707ad8df070041ae53c0c0ca9d3edd
liutaurasvilda <liutauras.vilda@gmail.com>
2021-12-04 13:38:18 +0000

    extract to methods

--- src/main/java/solutions/day04/Day04.java
+++ src/main/java/solutions/day04/Day04.java
@@ -11,12 +11,38 @@ final class Day04 {
 
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(74, 79, 46, 2, 19, 27, 31, 90, 21, 83, 94, 77, 0, 29, 38, 72, 42, 23, 6, 62, 45, 95, 41, 55, 93, 69, 39, 17, 12, 1, 20, 53, 49, 71, 61, 13, 88, 25, 87, 26, 50, 58, 28, 51, 89, 64, 3, 80, 36, 65, 57, 92, 52, 86, 98, 78, 9, 33, 44, 63, 16, 34, 97, 60, 40, 66, 75, 4, 7, 84, 22, 43, 11, 85, 91, 32, 48, 14, 18, 76, 8, 47, 24, 81, 35, 30, 82, 67, 37, 70, 15, 5, 73, 59, 54, 68, 56, 96, 99, 10);
+        List<BingoBoard> bingoBoards = populateBoards(ResourceReader.asString("day04.txt"));
+        List<Integer> winnerResults = playBingo(numbers, bingoBoards);
 
-        List<String> input = ResourceReader.asString("day04.txt");
+        System.out.println(part1(winnerResults));
+        System.out.println(part2(winnerResults));
+    }
 
+    private static int part1(List<Integer> winnerResults) {
+        return winnerResults.get(0);
+    }
+
+    private static int part2(List<Integer> winnerResults) {
+        return winnerResults.get(winnerResults.size()-1);
+    }
+
+    private static List<Integer> playBingo(List<Integer> numbers, List<BingoBoard> bingoBoards) {
+        List<Integer> winnerResults = new ArrayList<>();
+        for (int num : numbers) {
+            for (BingoBoard bingoBoard : bingoBoards) {
+                bingoBoard.mark(num);
+                if (bingoBoard.isWinner() && bingoBoard.wereNotWinnerBefore()) {
+                    winnerResults.add(bingoBoard.getScore());
+                    bingoBoard.setWereWinnerBefore();
+                }
+            }
+        }
+        return winnerResults;
+    }
+
+    private static List<BingoBoard> populateBoards(List<String> input) {
         List<BingoBoard> bingoBoards = new ArrayList<>();
         List<List<BingoNumber>> bingoNumbers = new ArrayList<>();
-
         for (String line : input) {
             if (line.isEmpty()) {
                 continue;
@@ -32,27 +58,6 @@ final class Day04 {
                 bingoNumbers = new ArrayList<>();
             }
         }
-
-        List<Integer> winnerResults = new ArrayList<>();
-        for (int num : numbers) {
-            for (BingoBoard bingoBoard : bingoBoards) {
-                bingoBoard.mark(num);
-                if (bingoBoard.isWinner() && bingoBoard.wereNotWinnerBefore()) {
-                    winnerResults.add(bingoBoard.getScore());
-                    bingoBoard.setWereWinnerBefore();
-                }
-            }
-        }
-
-        System.out.println(part1(winnerResults));
-        System.out.println(part2(winnerResults));
-    }
-
-    private static int part1(List<Integer> winnerResults) {
-        return winnerResults.get(0);
-    }
-
-    private static int part2(List<Integer> winnerResults) {
-        return winnerResults.get(winnerResults.size()-1);
+        return bingoBoards;
     }
 }
