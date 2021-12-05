7ada77ad79fc67e138228f02c8c1cb91a5684c2e
Liutauras Vilda <33405707+liutaurasvilda@users.noreply.github.com>
2021-12-05 14:58:15 +0000

    Update LineSegment.java

--- src/main/java/solutions/day05/LineSegment.java
+++ src/main/java/solutions/day05/LineSegment.java
@@ -40,11 +40,36 @@ final class LineSegment {
             }
         }
         else {
-            int minX = Math.min(a.x, b.x);
-            int maxX = Math.max(a.x, b.x);
-            int minY = Math.min(a.y, b.y);
-            int maxY = Math.max(a.y, b.y);
-            // todo
+            if (a.x == a.y && b.x == b.y && a.x < b.x) { // 1,1 -> 3,3
+                for (int i = a.x; i <= b.y; i++) {
+                    points.add(new Point(i, i));
+                }
+            }
+            else if (a.x == a.y && b.x == b.y && a.x > b.y) { // 3,3 -> 1,1
+                for (int i = a.x; i >= b.y; i--) {
+                    points.add(new Point(i, i));
+                }
+            }
+            else if (a.x != a.y && b.x != b.y && a.x < b.x && a.y < b.y) { // 1,2 -> 3,4
+                for (int i = 0; i <= b.x-a.x; i++) {
+                    points.add(new Point(a.x+i, a.y+i));
+                }
+            }
+            else if (a.x != a.y && b.x != b.y && a.x > b.x && a.y > b.y) { // 3,4 -> 1,2
+                for (int i = 0; i <= a.x-b.x; i++) {
+                    points.add(new Point(a.x-i, a.y-i));
+                }
+            }
+            else if (a.x > b.x && a.y < b.y) { // 9,7 -> 7,9
+                for (int i = 0; i <= a.x-b.x; i++) {
+                    points.add(new Point(a.x-i, a.y+i));
+                }
+            }
+            else if (a.x < b.x && a.y > b.y) { // 7,9 -> 9,7
+                for (int i = 0; i <= b.x-a.x; i++) {
+                    points.add(new Point(a.x+i, a.y-i));
+                }
+            }
         }
         return points;
     }
